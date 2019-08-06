b035e8dcc8b52ea2eb394faa5d47da7c63e9e330
Henry Leon Gomez <helg18@gmail.com>
2019-08-06 15:01:10 -0400

    Generando y registrando los componentes y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,28 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import {environment} from '../environments/environment';
 
+// Services
+import { EmpleadoService } from './services/empleado.service';
+
+// Components
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { ListComponent } from './components/empleados/list/list.component';
+import { EmpleadoComponent } from './components/empleados/empleado/empleado.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadosComponent,
+    ListComponent,
+    EmpleadoComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
   ],
-  providers: [],
+  providers: [
+    EmpleadoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
