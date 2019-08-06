3c4299bf53db7c07df3be00a1ba5e32c43195a5c
Henry Leon Gomez <helg18@gmail.com>
2019-08-06 15:14:28 -0400

    Configurando el servicio para poder usar la conexion de firebase y el modelo de empleado

--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import {Empleado} from '../models/empleado';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadoService {
 
-  constructor() { }
+  listaEmpleados: AngularFireList<Empleado>;
+
+  constructor(private firebase: AngularFireDatabase) { }
+
 }
